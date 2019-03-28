99e80ffaf7712e2d432dde0d8089843798398c75
Denis <denis.martsyniuk@gmail.com>
2019-03-28 21:49:23 +0200

    add init logic

---
+++ source/basic-redux/bus/gallery/actions.js
@@ -0,0 +1,10 @@
+import { SHOW_NEXT_PHOTO, SHOW_SELECTED_PHOTO } from "./types";
+
+export const showNextPhoto = () => ({
+  type: SHOW_NEXT_PHOTO
+});
+
+export const showSelectedPhoto = photoIndex => ({
+  type: SHOW_SELECTED_PHOTO,
+  payload: photoIndex
+});
