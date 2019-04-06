97eab80d7d2419db661b0ad668c89f09fd02721a
Denis <denis.martsyniuk@gmail.com>
2019-04-06 15:50:39 +0300

    add try catch

--- source/bus/posts/saga/workers/createPost.js
+++ source/bus/posts/saga/workers/createPost.js
@@ -4,8 +4,16 @@ import { api } from "../../../../REST";
 import { postsActions } from "../../actions";
 
 export function* createPost({ payload: comment }) {
-  const responce = yield apply(api, api.posts.create, [comment]);
-  const result = yield apply(responce, responce.json);
+  try {
+    const responce = yield apply(api, api.posts.create, [comment]);
+    const { data: post, message } = yield apply(responce, responce.json);
 
-  yield put(postsActions.fillNewPost(result.data));
+    if (responce !== 200) {
+      throw new Error(message);
+    }
+
+    yield put(postsActions.fillNewPost(post));
+  } catch (error) {
+    console.log(error);
+  }
 }
